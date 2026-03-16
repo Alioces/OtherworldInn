e4b8f7335225c6fb887d52f281041c538727e1f0
YOURZI <1841421236@qq.com>
2026-03-16 17:46:08 +0800

    feat: 添加异界旅社指南书及其本地化内容
    添加 Patchouli 模组的指南书支持，包含中英文双语的分类与条目。
    新增的指南书可通过合成获得，并在创造模式物品栏中直接提供。

--- src/main/java/com/otherworldinn/init/ModCreativeModeTabs.java
+++ src/main/java/com/otherworldinn/init/ModCreativeModeTabs.java
@@ -1,10 +1,16 @@
 package com.otherworldinn.init;
 
 import com.otherworldinn.OtherworldInn;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -33,6 +39,53 @@ public class ModCreativeModeTabs {
                                                 output.accept(ModItems.SPACE_SPHERE.get());
                                                 output.accept(ModItems.NETHER_SPACE_SPHERE.get());
                                                 output.accept(ModItems.END_SPACE_SPHERE.get());
+                                                BuiltInRegistries.ITEM
+                                                        .getOptional(
+                                                                ResourceLocation.parse(
+                                                                        "patchouli:guide_book"))
+                                                        .ifPresent(
+                                                                item -> {
+                                                                    ItemStack guideBook =
+                                                                            new ItemStack(item);
+                                                                    ResourceLocation bookId =
+                                                                            ResourceLocation.parse(
+                                                                                    "otherworldinn:otherworldinn_guide");
+                                                                    CompoundTag tag =
+                                                                            new CompoundTag();
+                                                                    tag.putString(
+                                                                            "patchouli:book",
+                                                                            bookId.toString());
+                                                                    guideBook.set(
+                                                                            DataComponents
+                                                                                    .CUSTOM_DATA,
+                                                                            CustomData.of(tag));
+                                                                    BuiltInRegistries
+                                                                            .DATA_COMPONENT_TYPE
+                                                                            .getOptional(
+                                                                                    ResourceLocation
+                                                                                            .parse(
+                                                                                                    "patchouli:book"))
+                                                                            .ifPresent(
+                                                                                    componentType -> {
+                                                                                        try {
+                                                                                            @SuppressWarnings(
+                                                                                                    "unchecked")
+                                                                                            DataComponentType<
+                                                                                                            ResourceLocation>
+                                                                                                    patchouliBookComponent =
+                                                                                                            (DataComponentType<
+                                                                                                                            ResourceLocation>)
+                                                                                                                    componentType;
+                                                                                            guideBook
+                                                                                                    .set(
+                                                                                                            patchouliBookComponent,
+                                                                                                            bookId);
+                                                                                        } catch (ClassCastException
+                                                                                                ignored) {
+                                                                                        }
+                                                                                    });
+                                                                    output.accept(guideBook);
+                                                                });
                                                 output.accept(ModBlocks.OVERWORLD_PORTAL.get());
                                             })
                                     .build());
