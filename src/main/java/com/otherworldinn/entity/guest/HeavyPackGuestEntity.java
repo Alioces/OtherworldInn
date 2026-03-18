c27fe5b7b60ada4c400de3bc984e7c2ea657e87b
YOURZI <1841421236@qq.com>
2026-03-18 23:57:44 +0800

    feat: 调整客人刷新规则并丰富重装客人的奖励池
    - 调整 RICH_GUEST、HEAVY_PACK_GUEST 和 ULTRA_RICH_GUEST 的刷新权重
    - 扩展 HeavyPackGuestEntity 的奖励物品池，包含多种农作物和食物
    - 使用加权随机算法从奖励池中选取 3 至 5 种物品作为实际奖励
    - 放宽重装客人的偏好范围和停留时间，使其行为更多样

--- src/main/java/com/otherworldinn/entity/guest/HeavyPackGuestEntity.java
+++ src/main/java/com/otherworldinn/entity/guest/HeavyPackGuestEntity.java
@@ -30,6 +30,38 @@ public class HeavyPackGuestEntity extends GuestEntity {
             ResourceLocation.fromNamespaceAndPath("create", "brass_ingot");
     private static final ResourceLocation PRECISION_MECHANISM_ID =
             ResourceLocation.fromNamespaceAndPath("create", "precision_mechanism");
+    private static final int HIGH_WEIGHT = 12;
+    private static final int MEDIUM_WEIGHT = 5;
+    private static final int ULTRA_LOW_WEIGHT = 1;
+    private static final List<RewardOption> REWARD_POOL = List.of(
+            new RewardOption(IRON_INGOT_ID, 0, 3, MEDIUM_WEIGHT),
+            new RewardOption(COPPER_INGOT_ID, 0, 4, MEDIUM_WEIGHT),
+            new RewardOption(ANDESITE_ALLOY_ID, 0, 2, MEDIUM_WEIGHT),
+            new RewardOption(ResourceLocation.fromNamespaceAndPath("minecraft", "wheat"), 0, 5, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "wheat_seeds"), 0, 6, HIGH_WEIGHT),
+            new RewardOption(ResourceLocation.fromNamespaceAndPath("minecraft", "carrot"), 0, 5, HIGH_WEIGHT),
+            new RewardOption(ResourceLocation.fromNamespaceAndPath("minecraft", "potato"), 0, 5, HIGH_WEIGHT),
+            new RewardOption(ResourceLocation.fromNamespaceAndPath("minecraft", "beetroot"), 0, 5, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "beetroot_seeds"), 0, 6, HIGH_WEIGHT),
+            new RewardOption(ResourceLocation.fromNamespaceAndPath("minecraft", "pumpkin"), 0, 2, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "pumpkin_seeds"), 0, 5, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "melon_slice"), 0, 6, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "melon_seeds"), 0, 5, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "sweet_berries"), 0, 6, HIGH_WEIGHT),
+            new RewardOption(ResourceLocation.fromNamespaceAndPath("minecraft", "apple"), 0, 3, HIGH_WEIGHT),
+            new RewardOption(ResourceLocation.fromNamespaceAndPath("minecraft", "bread"), 0, 3, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "baked_potato"), 0, 3, HIGH_WEIGHT),
+            new RewardOption(
+                    ResourceLocation.fromNamespaceAndPath("minecraft", "cooked_chicken"), 0, 2, HIGH_WEIGHT),
+            new RewardOption(BRASS_INGOT_ID, 0, 1, ULTRA_LOW_WEIGHT),
+            new RewardOption(PRECISION_MECHANISM_ID, 0, 1, ULTRA_LOW_WEIGHT));
     private static final List<ResourceLocation> TEXTURES = new ArrayList<>();
     private static boolean texturesLoaded = false;
 
@@ -40,19 +72,34 @@ public class HeavyPackGuestEntity extends GuestEntity {
     @Override
     protected GuestProfile getGuestProfile() {
         return new GuestProfile(
-                new PreferenceRangeProfile(new GuestData.IntRange(28, 42), new GuestData.IntRange(72, 88)),
-                new PreferenceRangeProfile(new GuestData.IntRange(30, 45), new GuestData.IntRange(74, 90)),
-                new PreferenceRangeProfile(new GuestData.IntRange(25, 40), new GuestData.IntRange(70, 86)),
-                new GuestData.IntRange(14, 34));
+                new PreferenceRangeProfile(new GuestData.IntRange(18, 42), new GuestData.IntRange(62, 88)),
+                new PreferenceRangeProfile(new GuestData.IntRange(20, 45), new GuestData.IntRange(54, 90)),
+                new PreferenceRangeProfile(new GuestData.IntRange(15, 40), new GuestData.IntRange(50, 86)),
+                new GuestData.IntRange(12, 55));
     }
 
     @Override
     protected void initRewardItems() {
-        this.getGuestData().addRewardItem(IRON_INGOT_ID, 0, 3);
-        this.getGuestData().addRewardItem(COPPER_INGOT_ID, 0, 4);
-        this.getGuestData().addRewardItem(ANDESITE_ALLOY_ID, 0, 2);
-        this.getGuestData().addRewardItem(BRASS_INGOT_ID, 0, 1);
-        this.getGuestData().addRewardItem(PRECISION_MECHANISM_ID, 0, 1);
+        List<RewardOption> options = new ArrayList<>(REWARD_POOL);
+        int count = 3 + this.getRandom().nextInt(3);
+        int selectedCount = Math.min(count, options.size());
+        for (int i = 0; i < selectedCount; i++) {
+            int totalWeight = 0;
+            for (RewardOption option : options) {
+                totalWeight += option.weight();
+            }
+            int roll = this.getRandom().nextInt(totalWeight);
+            int weightedIndex = 0;
+            for (int idx = 0; idx < options.size(); idx++) {
+                roll -= options.get(idx).weight();
+                if (roll < 0) {
+                    weightedIndex = idx;
+                    break;
+                }
+            }
+            RewardOption option = options.remove(weightedIndex);
+            this.getGuestData().addRewardItem(option.itemId(), option.minCount(), option.maxCount());
+        }
     }
 
     @Override
@@ -92,4 +139,6 @@ public class HeavyPackGuestEntity extends GuestEntity {
         this.setSkinVariant(this.getRandom().nextInt(10000));
         return spawnData;
     }
+
+    private record RewardOption(ResourceLocation itemId, int minCount, int maxCount, int weight) {}
 }
